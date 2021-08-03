59142b8f95d4e15d329c22fc617eecbdb933adeb
Mateo Serna Lopera <mateo.serna@compurent.com.co>
2021-08-03 16:57:22 -0500

    V 0.0.1

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
+  images: {
+    domains: [
+      'links.papareact.com',
+      'image.tmdb.org'
+    ]
+  },
   reactStrictMode: true,
 }
--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+const user = {
+  id: 1,
+  name: 'Mateo',
+  lastName: 'Serna',
+  role: 'Developer',
+  password: 'ja#*"3k#$%##""$#sd',
+}
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json(user)
 }
